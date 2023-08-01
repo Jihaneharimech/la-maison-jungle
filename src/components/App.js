f2918f6ac557fffbe9cf324a66e31321b8218f7e
jihane <jihane.harimech@gmail.com>
2023-08-01 15:24:50 +0200

    Creation componoet plantTem.js, planttem.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Banner from './Banner'
 //import Cart from './Cart'
 import ShoppingList from './ShoppingList'
+import CareScale from './CareScale'
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Banner />
 			{/*<Cart />*/}
 			<ShoppingList />
+			<CareScale />
 		</div>
 	)
 }
